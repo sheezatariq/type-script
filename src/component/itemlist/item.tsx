e71c774cad3a816124e66d1fa2e3eea8fb8d1cec
Sheeza Tariq <sheezatariqnaina9@gmail.com>
2020-09-08 12:48:33 +0500

    changes of drag and drop

--- src/component/itemlist/item.tsx
+++ src/component/itemlist/item.tsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './item.css';
+import RLDD from 'react-list-drag-and-drop/lib/RLDD';
 
 const Item = () => {
   const history = useHistory()
-
-  const [item, setItem] = React.useState<string>("");
-  const [list, setList] = React.useState<string[]>([]);
-
+  const [item, setItem] = useState<string>("");
+  const [list, setList] = useState([] as any);
+  
   const handleChange = (e: any) => {
     setItem(e.target.value);
   };
 
-  const addObject = (e: any) => {
-    setList([...list, item]);
+  const addObject = () => {
+    setList([...list, { id: list.length !== 0 ? (list.length - 1) + 1 : list.length, item }]);
     setItem('');
   };
 
   const logout = () => {
     history.push('/login')
   };
-  
+
+  const handleRldChange = (newItems : any) => {
+    setList(newItems);
+  }
+
   return (
     <div className="field">
-      <div className='input-field'>
+      <div className="input-field">
         <input type="text"  value={item} onChange={handleChange} placeholder="Enter Text" />
         <button disabled={item.length<=0} onClick={addObject}>Add</button>
         <button onClick={logout}>LOGOUT</button>
         <div className="lis">
-          {list && list.map((itm, index) => (
-            <div className="list-style">
-              {itm}
-            </div>
-          ))}
+          <RLDD
+            items={list}
+            itemRenderer={(item: any) => {
+              return (
+                <div className="list-style">
+                  {item?.item}
+                </div>
+              );
+            }}
+            onChange={handleRldChange}
+          />
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
